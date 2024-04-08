80bb3d328e5aec143467fc940c0ede63eda9b95e
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2024-04-08 20:55:23 +0200

    feat(extension): Update config in manifest and implement content.

--- custom-webpack.config.ts
+++ custom-webpack.config.ts
@@ -1,5 +1,11 @@
 import type { Configuration } from 'webpack';
 
 module.exports = {
-  entry: { background: 'src/background.ts' },
+  entry: {
+    background: 'src/background.ts',
+    'content-script': 'src/content.ts',
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
 } as Configuration;
