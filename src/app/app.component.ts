c63024a2c4c2e2b134d443ef13651fc97c87591d
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2024-04-09 17:42:38 +0200

    feat(app): Interact with dom. Create 4 new functions with some basic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,32 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-extension';
+
+  public darkBackground() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id || 0, { type: 'change-color' });
+    });
+  }
+
+  public addButton() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id || 0, { type: 'add-button' });
+    });
+  }
+
+  public replaceBody() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id || 0, { type: 'replace-body' });
+    });
+  }
+
+  public replaceTitles() {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id || 0, { type: 'replace-titles' });
+    });
+  }
 }
---
+++ src/content.ts
@@ -0,0 +1,37 @@
+console.log('Content script working');
+
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type === 'change-color') {
+    document.body.style.backgroundColor = 'black';
+  } else if (message.type === 'replace-body') {
+    document.body.innerHTML = '<h1>Hello I am the new body :D</h1>';
+  } else if (message.type === 'add-button') {
+    var newButton = document.createElement('button');
+    newButton.id = 'bodyButton';
+    newButton.type = 'button';
+    newButton.textContent = 'A new button';
+    document.body.appendChild(newButton);
+  } else if (message.type === 'replace-titles') {
+    var h1Texts = document.querySelectorAll('h1');
+    var h2Texts = document.getElementsByTagName('h2');
+    var h3Texts = document.getElementsByTagName('h3');
+    for (var index in h1Texts) {
+      if (h1Texts[index] && h1Texts[index].style) {
+        h1Texts[index].style.fontWeight = 'bolder';
+        h1Texts[index].style.color = 'green';
+      }
+    }
+    for (var index in h2Texts) {
+      if (h2Texts[index] && h2Texts[index].style) {
+        h2Texts[index].style.fontWeight = 'bold';
+        h2Texts[index].style.color = 'red';
+      }
+    }
+    for (var index in h3Texts) {
+      if (h3Texts[index] && h3Texts[index].style) {
+        h3Texts[index].style.fontWeight = 'bold';
+        h3Texts[index].style.color = 'blue';
+      }
+    }
+  }
+});
